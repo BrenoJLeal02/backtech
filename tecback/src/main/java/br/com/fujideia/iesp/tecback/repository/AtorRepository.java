11ce54b32c1dcfaef9c7f5f4c85b51e6f3be15e0
BrenoJLeal02 <copywriting.leal@gmail.com>
2024-09-10 17:14:41 -0300

    Delete tecback directory

--- tecback/src/main/java/br/com/fujideia/iesp/tecback/repository/AtorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fujideia.iesp.tecback.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import br.com.fujideia.iesp.tecback.model.Ator;
-@Repository
-public interface AtorRepository extends JpaRepository<Ator, Long> {
-}
--- tecback/src/main/java/br/com/fujideia/iesp/tecback/repository/DiretorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fujideia.iesp.tecback.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import br.com.fujideia.iesp.tecback.model.Diretor;
-@Repository
-public interface DiretorRepository extends JpaRepository<Diretor, Long> {
-}
